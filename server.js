fd075ba97b84f92c15525657ce719d76ea6ecd24
x-rodionov <code512edu@gmail.com>
2022-03-30 21:08:03 +0530

    get content api call bug fix 2

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ runQuery(queryString).then( async (result) => {
         const id = req.params.knit.split('=')[1];
         const contentRecord = await getContentRecord(id);
         console.log(contentRecord);
-        const {text, author} = contentRecord;
+        const {text, author} = contentRecord[0];
         const apiCallTotalPrice = apiCallPrice.base + apiCallPrice.perSymbol * text.length;
         await hostingFeeTransfer(req.user.id, defaultHostingProvider.id, apiCallPrice);
         //await authorFeeTransfer(req.user.id, author, contentViewPrice);
