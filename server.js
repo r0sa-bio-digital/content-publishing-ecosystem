182dc4cceac313aaea1aa59a596155d0c5687d04
x-rodionov <code512edu@gmail.com>
2022-03-31 17:55:23 +0530

    apiCallId added to transaction_log records

--- server.js
+++ server.js
@@ -40,15 +40,11 @@ async function runQuery(queryString) {
     }
     return result;
 }
-
-
-
-
-async function hostingFeeTransfer(userId, hostingProviderId, amount, contentId) {
+async function hostingFeeTransfer(userId, hostingProviderId, amount, contentId, apiCallId) {
     const transactionId = knit.generate();
     const contentIdValue = contentId ? '\'' + contentId + '\'' : 'NULL';
-    const queryString = 'INSERT INTO "public"."transaction_log" ("id", "debited_account", "credited_account", "c01n_amount", "content_id") ' +
-        'VALUES (\'' + transactionId + '\', \'' + userId + '\', \'' + hostingProviderId + '\', ' + amount + ', ' + contentIdValue + ');\n' +
+    const queryString = 'INSERT INTO "public"."transaction_log" ("id", "debited_account", "credited_account", "c01n_amount", "content_id", "api_call_id") ' +
+        'VALUES (\'' + transactionId + '\', \'' + userId + '\', \'' + hostingProviderId + '\', ' + amount + ', ' + contentIdValue + ', \'' + apiCallId + '\');\n' +
         'UPDATE "public"."users" SET "balance" = "balance" - ' + amount + ' WHERE "id" = \'' + userId + '\';\n' +
         'UPDATE "public"."hosting_providers" SET "balance" = "balance" + ' + amount + ' WHERE "id" = \'' + hostingProviderId + '\';';
     await runQuery(queryString);
@@ -91,21 +87,24 @@ runQuery(queryString).then( async (result) => {
     }
     // define api calls
     app.get('/knit/generate', auth.user, async (req, res) => {
+        const apiCallId = '95f37a03-c1c7-41fe-bead-33c4536b0a2b';
         const apiCallPrice = 1000;
         const resultKnit = knit.generate();
-        await hostingFeeTransfer(req.user.id, defaultHostingProvider.id, apiCallPrice);
+        await hostingFeeTransfer(req.user.id, defaultHostingProvider.id, apiCallPrice, undefined, apiCallId);
         res.set('Content-Type', 'text/html');
         res.send(resultKnit);
     });
     app.get('/:knit/extract/timestamp/', auth.user, async (req, res) => {
+        const apiCallId = '95f37a28-aa0e-4a73-a833-7a707144f5ce';
         const apiCallPrice = 1000;
         const id = req.params.knit.split('=')[1];
         const resultTimestamp = knit.convertTime(id, 'date-object');
-        await hostingFeeTransfer(req.user.id, defaultHostingProvider.id, apiCallPrice);
+        await hostingFeeTransfer(req.user.id, defaultHostingProvider.id, apiCallPrice, undefined, apiCallId);
         res.set('Content-Type', 'text/html');
         res.send(resultTimestamp.toISOString());
     });
     app.get('/:knit', auth.user, async (req, res) => {
+        const apiCallId = '95f37a3f-19ad-448e-bd56-04a8da5c0df4';
         const apiCallPrice = {base: 10000, perSymbol: 10};
         const id = req.params.knit.split('=')[1];
         const contentRecord = (await getContentRecord(id))[0];
@@ -113,14 +112,14 @@ runQuery(queryString).then( async (result) => {
         {
             const {text, author, author_fee} = contentRecord;
             const apiCallTotalPrice = apiCallPrice.base + apiCallPrice.perSymbol * text.length;
-            await hostingFeeTransfer(req.user.id, defaultHostingProvider.id, apiCallTotalPrice, id);
+            await hostingFeeTransfer(req.user.id, defaultHostingProvider.id, apiCallTotalPrice, id, apiCallId);
             await authorFeeTransfer(req.user.id, author, author_fee);
             res.set('Content-Type', 'text/html');
             res.send(text);
         }
         else
         {
-            await hostingFeeTransfer(req.user.id, defaultHostingProvider.id, apiCallPrice.base);
+            await hostingFeeTransfer(req.user.id, defaultHostingProvider.id, apiCallPrice.base, undefined, apiCallId);
             res.status(404).json({ message: 'Content not found' });
         }
     });
