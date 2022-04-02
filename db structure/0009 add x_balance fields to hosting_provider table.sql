7c9ceb5b84348149e59b0daf0b9e8a6244de3a48
Venera Rodionova <c0ntent.dao.kz@gmail.com>
2022-04-02 21:24:27 +0530

    add inr, cny, usd and eur currencies to rub

---
+++ db structure/0009 add x_balance fields to hosting_provider table.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "public"."hosting_providers"
+ADD COLUMN "95f5ebe7-c7a2-4283-bb48-96fcbe53a65c_balance" numeric NOT NULL DEFAULT 0,
+ADD COLUMN "95f7cac7-13ae-4872-b69e-b64802612633_balance" numeric NOT NULL DEFAULT 0,
+ADD COLUMN "95f7cae1-af63-4a6e-a2d2-d7f968694a7a_balance" numeric NOT NULL DEFAULT 0,
+ADD COLUMN "95f7caf7-24eb-4612-846a-f3dec4afc741_balance" numeric NOT NULL DEFAULT 0,
+ADD COLUMN "95f7cb0b-e1d2-4e33-98f3-50bcf2b57c7c_balance" numeric NOT NULL DEFAULT 0;
